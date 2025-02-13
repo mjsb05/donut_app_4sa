5a9c73b7460129fd2f9d3cb84a45a359aa1be758
mjsb05 <maurijsb25@gmail.com>
2025-02-13 13:32:31 -0600

    main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,19 +11,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Scaffold es un widget que implementa la estructura visual básica de material design
-      appBar: AppBar(
-          //AppBar es un widget que implementa la parte superior de la aplicación
-          backgroundColor: Colors.transparent,
-          //icóno de la izquierda
-          leading: Icon(
-            Icons.menu,
-            color: Colors.grey[800],
-          ),
-          actions: [Icon(Icons.person)]),
-      body: const Center(
-        child: Text('Home Page'),
-      ),
-    );
+        //Scaffold es un widget que implementa la estructura visual básica de material design
+        appBar: AppBar(
+            //AppBar es un widget que implementa la parte superior de la aplicación
+            backgroundColor: Colors.transparent,
+            //icóno de la izquierda
+            leading: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
+                child: Icon(Icons.person),
+              )
+            ]),
+        body: Column(
+          children: [
+            //1.Texto principal(Text),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Row(
+                children: [
+                  Text('I want to ', style: TextStyle(fontSize: 32)),
+                  Text(
+                    'Eat',
+                    style: TextStyle(
+                      //tamaño de texto
+                      fontSize: 32,
+                      //Negritas
+                      fontWeight: FontWeight.bold,
+                      //subrayado
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
