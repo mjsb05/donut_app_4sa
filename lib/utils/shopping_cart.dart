72e136b53363a0e326758f61bb6ea02d071767a3
mjsb05 <maurijsb25@gmail.com>
2025-04-03 13:27:07 -0600

    Fix commits

---
+++ lib/utils/shopping_cart.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCart extends StatelessWidget {
+  const ShoppingCart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Padding(
+                padding: EdgeInsets.only(left: 28),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('2 items | \$45 .00',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Text("Delivery Changes Incluided")
+                  ],
+                )),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12)),
+                child: const Text("View Cart",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )))
+          ],
+        ));
+  }
+}
