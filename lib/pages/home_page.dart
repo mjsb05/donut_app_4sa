8b8ce8b2c0b0a92d70c50027ffdf10175f6d86d7
mjsb05 <maurijsb25@gmail.com>
2025-04-03 13:15:40 -0600

    ShoppingCart added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:donut_app_4sa/tabs/pancakes_tab.dart';
 import 'package:donut_app_4sa/tabs/pizza_tab.dart';
 import 'package:donut_app_4sa/tabs/smoothie_tab.dart';
 import 'package:donut_app_4sa/utils/my_tab.dart';
+import 'package:donut_app_4sa/utils/shopping_cart.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -77,6 +78,8 @@ class _HomePageState extends State<HomePage> {
                   PizzaTab(),
                 ]),
               ),
+              //carrito
+              ShoppingCart()
             ],
           )),
     );
