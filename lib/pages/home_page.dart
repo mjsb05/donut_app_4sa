d693fd5c2f93d9a563523d036d8f3e0a253e49de
mjsb05 <maurijsb25@gmail.com>
2025-04-03 13:21:29 -0600

    Revert "ShoppingCart added"
    This reverts commit 8b8ce8b2c0b0a92d70c50027ffdf10175f6d86d7.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'package:donut_app_4sa/tabs/pancakes_tab.dart';
 import 'package:donut_app_4sa/tabs/pizza_tab.dart';
 import 'package:donut_app_4sa/tabs/smoothie_tab.dart';
 import 'package:donut_app_4sa/utils/my_tab.dart';
-import 'package:donut_app_4sa/utils/shopping_cart.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -78,8 +77,6 @@ class _HomePageState extends State<HomePage> {
                   PizzaTab(),
                 ]),
               ),
-              //carrito
-              ShoppingCart()
             ],
           )),
     );
